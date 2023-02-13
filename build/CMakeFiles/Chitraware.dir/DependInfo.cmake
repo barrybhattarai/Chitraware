3616138d9098a27cc16619ee560713708d6277c8
Barry Bhattarai <bhanubhattarai13@gmail.com>
2023-02-13 08:47:44 +0530

    Added CMakeLists.txt

---
+++ build/CMakeFiles/Chitraware.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/barry/CodeParty/Chitraware/main.cpp" "CMakeFiles/Chitraware.dir/main.cpp.o" "gcc" "CMakeFiles/Chitraware.dir/main.cpp.o.d"
+  "/home/barry/CodeParty/Chitraware/renderer.cpp" "CMakeFiles/Chitraware.dir/renderer.cpp.o" "gcc" "CMakeFiles/Chitraware.dir/renderer.cpp.o.d"
+  "/home/barry/CodeParty/Chitraware/shader.cpp" "CMakeFiles/Chitraware.dir/shader.cpp.o" "gcc" "CMakeFiles/Chitraware.dir/shader.cpp.o.d"
+  "/home/barry/CodeParty/Chitraware/vertexBuffer.cpp" "CMakeFiles/Chitraware.dir/vertexBuffer.cpp.o" "gcc" "CMakeFiles/Chitraware.dir/vertexBuffer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
